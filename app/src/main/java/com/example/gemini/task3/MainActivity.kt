1fb27817f5cb1c330ccbe286943fd46acb2f404f
DavBlain <davblain123@gmail.com>
2018-02-13 15:58:00 +0300

    original of photo in view

--- app/src/main/java/com/example/gemini/task3/MainActivity.kt
+++ app/src/main/java/com/example/gemini/task3/MainActivity.kt
@@ -2,29 +2,51 @@ package com.example.gemini.task3
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_IMAGE_CAPTURE = 1
-
+    private var createdFile:File? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        makePhotoButton.setOnClickListener {
-           val makePictureIntent =  Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(makePictureIntent,REQUEST_IMAGE_CAPTURE)
-        }
+        makePhotoButton.setOnClickListener { makePhoto() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val extras = data?.extras
-            startActivity(PhotoPreviewActivity.newIntent(this,extras?.get("data") as Bitmap,nameTextEdit.text.toString()))
+            startActivity(PhotoPreviewActivity.newIntent(this, createdFile!!.absolutePath, nameTextEdit.text.toString()))
         }
 
+    }
+    private fun  makePhoto() {
+        createdFile = createPhotoFile()
+        val  photoUri = FileProvider.getUriForFile(this,"com.gemini.fileprovider",createdFile)
+        val makePictureIntent =  Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        makePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
+        startActivityForResult(makePictureIntent,REQUEST_IMAGE_CAPTURE)
+    }
+
+    private fun createPhotoFile():File? {
+        if (Environment.getExternalStorageState()==Environment.MEDIA_MOUNTED) {
+            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val imageFileName = "JPEG_" + timeStamp + "_"
+            val mediaStorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            return File.createTempFile(imageFileName,
+                    ".jpg",
+                    mediaStorageDir)
+        }
+        return null
     }
 }
--- app/src/main/java/com/example/gemini/task3/PhotoPreviewActivity.kt
+++ app/src/main/java/com/example/gemini/task3/PhotoPreviewActivity.kt
@@ -3,9 +3,11 @@ package com.example.gemini.task3
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_photo_preview.*
+import java.io.File
 
 class PhotoPreviewActivity : AppCompatActivity() {
 
@@ -13,17 +15,17 @@ class PhotoPreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_preview)
-        photoImage.setImageBitmap(intent.extras[PHOTO] as Bitmap)
+        photoImage.setImageBitmap(BitmapFactory.decodeFile(intent.extras[PHOTO_PATH] as String))
         signatureTextView.text = intent.extras[NAME] as String
     }
 
     companion object {
 
-        private val PHOTO = "photo";
+        private val PHOTO_PATH = "photo";
         private val NAME = "name"
-        fun newIntent(context: Context,photo:Bitmap, name: String): Intent {
+        fun newIntent(context: Context,photo:String, name: String): Intent {
             val intent = Intent(context, PhotoPreviewActivity::class.java)
-            intent.putExtra(PHOTO, photo)
+            intent.putExtra(PHOTO_PATH, photo)
             intent.putExtra(NAME,name)
             return intent
         }
